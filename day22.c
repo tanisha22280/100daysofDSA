3755add086474185d6622b4211ddf43aed4bb13d
tanisha22280 <Tanisha.22280@stu.upes.ac.in>
2026-03-27 22:06:52 +0530

    Add files via upload

---
+++ day22.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n, i, value;
+
+    scanf("%d", &n);
+
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &value);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if(head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    int count = 0;
+    temp = head;
+
+    while(temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
