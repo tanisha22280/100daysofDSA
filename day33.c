598f3580f3c9e0d039bb4abf0bb96b397eefb3b8
tanisha22280 <Tanisha.22280@stu.upes.ac.in>
2026-03-28 09:39:59 +0530

    Add files via upload

---
+++ day33.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char c) {
+    stack[++top] = c;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+char peek() {
+    return stack[top];
+}
+
+int precedence(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    return 0;
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+    int k = 0;
+
+    scanf("%s", infix);
+
+    for (int i = 0; i < strlen(infix); i++) {
+        char ch = infix[i];
+
+        if (isalnum(ch)) {
+            postfix[k++] = ch;
+        }
+        else if (ch == '(') {
+            push(ch);
+        }
+        else if (ch == ')') {
+            while (top != -1 && peek() != '(') {
+                postfix[k++] = pop();
+            }
+            pop(); // remove '('
+        }
+        else {
+            while (top != -1 && precedence(peek()) >= precedence(ch)) {
+                postfix[k++] = pop();
+            }
+            push(ch);
+        }
+    }
+
+    while (top != -1) {
+        postfix[k++] = pop();
+    }
+
+    postfix[k] = '\0';
+
+    printf("%s", postfix);
+
+    return 0;
+}
