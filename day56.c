4ed1c230daf9dc48c123667ddab2b140d2ab028a
tanisha22280 <Tanisha.22280@stu.upes.ac.in>
2026-03-30 01:17:55 +0530

    Add files via upload

---
+++ day56.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* createNode(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1)
+        return NULL;
+
+    struct Node* root = createNode(arr[0]);
+    struct Node* queue[n];
+    int front = 0, rear = 0;
+    queue[rear++] = root;
+
+    int i = 1;
+
+    while (i < n) {
+        struct Node* curr = queue[front++];
+
+        if (i < n && arr[i] != -1) {
+            curr->left = createNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+
+        if (i < n && arr[i] != -1) {
+            curr->right = createNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+
+    return root;
+}
+
+int isMirror(struct Node* t1, struct Node* t2) {
+    if (t1 == NULL && t2 == NULL)
+        return 1;
+
+    if (t1 == NULL || t2 == NULL)
+        return 0;
+
+    return (t1->data == t2->data) &&
+           isMirror(t1->left, t2->right) &&
+           isMirror(t1->right, t2->left);
+}
+
+int isSymmetric(struct Node* root) {
+    if (root == NULL)
+        return 1;
+
+    return isMirror(root->left, root->right);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* root = buildTree(arr, n);
+
+    if (isSymmetric(root))
+        printf("YES\n");
+    else
+        printf("NO\n");
+
+    return 0;
+}
