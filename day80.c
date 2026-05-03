e7ad06ac7f0f9387a1820c8b068d0e5e58cf97fc
tanisha22280 <Tanisha.22280@stu.upes.ac.in>
2026-05-03 22:05:26 +0530

    Add files via upload

---
+++ day80.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+#define MAX 100
+#define INF 1000000000
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int dist[MAX][MAX];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &dist[i][j]);
+
+            if (dist[i][j] == -1 && i != j) {
+                dist[i][j] = INF;
+            }
+        }
+    }
+
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (dist[i][k] < INF && dist[k][j] < INF) {
+                    if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+
+            if (dist[i][j] == INF)
+                printf("-1 ");
+            else
+                printf("%d ", dist[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
